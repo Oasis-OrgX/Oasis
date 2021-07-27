343dc63abbd32791dbc383145ea5125d02cd2493
Bhavesh <bhic2030@gmail.com>
2021-07-27 19:26:30 +0530

    Initial commit - Project Setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = ()=>{
+    return (
+      <>
+        <div className="container">
+          <h1>
+            your project setup was done ! <br />
+            - Oasis
+          </h1>
+        </div>
+      </>
+    );
+}
+
+export default App;
