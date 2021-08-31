db798a7108e1d19010a445ba383d40016468e3c7
Bhavesh <bhic2030@gmail.com>
2021-08-31 15:12:04 +0530

    restoring previous version

--- client/src/components/features/feature-data.js
+++ client/src/components/features/feature-data.js
@@ -15,39 +15,31 @@ import fetaure4Image from "./features-img/feature4.jpg";
 
 export default [
   {
-    id: 0,
     imgSrc: fetaure1Image,
     featureTextIconName: "Timetable",
     featureIcon: timeTableImage,
     text: "Now never miss your class with access to live time table, class and attendance links provided all at one place. It will make you punctual, with no more roaming around the whatsApp groups and searching for essential links.",
-    style: "row",
-    borderColor: "10px solid rgba(0,255,0,.5)",
+    style: "featureReverse",
   },
   {
-    id: 1,
     imgSrc: fetaure2Image,
     featureTextIconName: "Quick Updates",
     featureIcon: updatesImage,
     text: "Get Updates of everything happening at one place. Never miss out even minutes of updates of things happening around in your college.",
-    style: "row-reverse",
-    borderColor: "10px solid rgba(255,0,0,.5)",
+    style: "feature",
   },
   {
-    id: 2,
     imgSrc: fetaure3Image,
     featureTextIconName: "Deadlines",
     featureIcon: deadlinesImage,
     text: "Get deadlines of every assignment, projects and many more so you never miss any deadlines and keep up with others ",
-    style: "row",
-    borderColor: "10px solid rgba(0,120,120,.5)",
+    style: "featureReverse",
   },
   {
-    id: 3,
     imgSrc: fetaure4Image,
     featureTextIconName: "Resources",
     featureIcon: resourcesImage,
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis ut quos ullam harum a cumque omnis accusamus unde quod fugiat possimus voluptate deleniti cum sed aspernatur, voluptatum laudantium doloribus totam consequatur, autem asperiores, ipsam maxime perspiciatis qui! Ducimus enim tempore eius consequuntur ea esse! Vitae enim minus facilis voluptas voluptatibus.",
-    style: "row-reverse",
-    borderColor: "10px solid rgba(0,0,255,.5)",
+    style: "feature",
   },
 ];
--- client/src/pages/guestpage.js
+++ client/src/pages/guestpage.js
@@ -7,7 +7,6 @@ import Video from "../components/video/video";
 import Footer from "../components/footer/footer";
 // Importing Data
 import featuresData from "../components/features/feature-data";
-import { Grid } from "@material-ui/core";
 
 console.log(featuresData);
 
@@ -20,25 +19,14 @@ const GuestPage = () => {
         <div className="feature-heading">
           <h1>About Oasis</h1>
         </div>
-        {/* <div className="feature-components">
+        <div className="feature-components">
           {featuresData.map((feature, index) => {
             return <Feature key={index} {...feature}></Feature>;
           })}
-        </div> */}
-        <Grid
-          Box
-          spacing={10}
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          {featuresData.map((feature, index) => {
-            return <Feature key={index} {...feature}></Feature>;
-          })}
-        </Grid>
+        </div>
       </div>
-      {/* <Video></Video> */}
-      <Footer></Footer>
+      {/* <Video></Video>
+        <Footer></Footer> */}
     </>
   );
 };
